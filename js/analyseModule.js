3af3fe834bf323d150acee09642319134ee02919
bprotsyk <nesterov.dev@gmail.com>
2018-01-22 14:18:26 +0200

    Added analyse module

---
+++ js/analyseModule.js
@@ -0,0 +1,36 @@
+InLike.isBodyLoaded = function() {
+  return typeof document.getElementsByTagName("body")[0] != "undefined";
+};
+
+//in future implement timer (to track if page didn't load)
+InLike.analyse = function() {
+  var isNotLoggedIn = function() {
+    return typeof $(".not-logged-in")[0] != "undefined";
+  };
+
+  if (!InLike.isBodyLoaded()) {
+    setTimeout(function() {
+      InLike.analyse();
+    }, 300); // waiting 300 ms for body to load
+  } else {
+    if (isNotLoggedIn()) {
+      console.log("Not logged in!");
+    } else {
+      console.log("Logged in");
+    }
+
+    // if (document.getElementById("mainFeed") != null) {
+    //   InLikeHandler.onPageLoaded("main");
+    // } else if (document.getElementsByName("emailOrPhone").length > 0) {
+    //   InLikeHandler.onPageLoaded("sign_up");
+    // } else {
+    //   InLikeHandler.onPageLoaded("log_in");
+    // }
+  }
+};
+
+if (!InLike.isBodyLoaded()) {
+  window.onload = function() {
+    InLike.analyse();
+  };
+} else InLike.analyse();
